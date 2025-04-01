9e0fe2edc8153ac0462ebca4a6fe8e3bf7a1b3df
MatthieuSoftware <matthieujeanpierre5@gmail.com>
2025-04-01 20:04:22 +0200

    Create basis for our first  application
    it include mathmatical operatorion

---
+++ app.py
@@ -0,0 +1,24 @@
+x = 150
+y = 100
+
+z = x + y 
+print(x)
+print(y)
+#python  is using indents
+x = 10
+b = 5
+c = a / b 
+d = a * b 
+e = a % b 
+
+a == b
+a || b 
+a === b 
+a != b 
+a ^^ b 
+a && b 
+name = "Anna"
+surname = "Grey"
+
+student_name = name + "  " +surname 
+print(student)
